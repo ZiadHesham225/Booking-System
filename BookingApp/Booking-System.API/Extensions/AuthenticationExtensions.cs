0c7587fbcfb33579c9bb9dd5929a172a89eadf96
ZiadHesham225 <heshamziad391@gmail.com>
2026-01-06 20:53:15 +0200

    fix: update JWT config

--- BookingApp/Booking-System.API/Extensions/AuthenticationExtensions.cs
+++ BookingApp/Booking-System.API/Extensions/AuthenticationExtensions.cs
@@ -26,23 +26,8 @@ namespace Booking_System.Extensions
                     ValidateIssuerSigningKey = true,
                     ValidIssuer = configuration["JWT:ValidIssuer"],
                     ValidAudience = configuration["JWT:ValidAudience"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"])),
-                };
-
-                options.Events = new JwtBearerEvents
-                {
-                    OnMessageReceived = context =>
-                    {
-                        var accessToken = context.Request.Query["access_token"];
-                        var path = context.HttpContext.Request.Path;
-
-                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/chat"))
-                        {
-                            context.Token = accessToken;
-                        }
-
-                        return Task.CompletedTask;
-                    }
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]!)),
+                    ClockSkew = TimeSpan.Zero
                 };
             });
 
